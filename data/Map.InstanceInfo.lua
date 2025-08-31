b1b23ac6d4e42e0c92c7cff3613d23047e1d58ce
Can Yilmaz <can@yilmaz.app>
2025-08-31 23:28:14 +0100

    proper dungeon data

---
+++ data/Map.InstanceInfo.lua
@@ -0,0 +1,698 @@
+setfenv(1, GoggleMaps)
+GoggleMaps.Map.InstanceInfo = {
+	[50001] = {
+		ownerMapId = 1001,
+		entryX = 14.15,
+		entryY = 13.9,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "blackfathomdeeps"
+			}
+		}
+	},
+	[50002] = {
+		ownerMapId = 2006,
+		entryX = 23.88,
+		entryY = 9.6,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "blackrockdepths"
+			},
+			{
+				x = 0,
+				y = -100,
+				overlay = "blackrockdepths2f"
+			}
+		}
+	},
+	[50003] = {
+		ownerMapId = 2006,
+		entryX = 66,
+		entryY = 41.5,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "blackrockspire"
+			},
+			{
+				x = 0,
+				y = -100,
+				overlay = "blackrockspire2f"
+			},
+			{
+				x = 0,
+				y = -200,
+				overlay = "blackrockspire3f"
+			},
+			{
+				x = 0,
+				y = -300,
+				overlay = "blackrockspire4f"
+			},
+			{
+				x = 0,
+				y = -400,
+				overlay = "blackrockspire5f"
+			},
+			{
+				x = 0,
+				y = -500,
+				overlay = "blackrockspire6f"
+			},
+			{
+				x = 0,
+				y = -600,
+				overlay = "blackrockspire7f"
+			}
+		}
+	},
+	[50004] = {
+		ownerMapId = 1001,
+		entryX = 51.4,
+		entryY = 78,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "crescentgrove"
+			}
+		}
+	},
+	[50005] = {
+		ownerMapId = 1012,
+		entryX = 59.13,
+		entryY = 45.16,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "diremaul"
+			},
+			{
+				x = 0,
+				y = -100,
+				overlay = "diremaul2f"
+			},
+			{
+				x = 0,
+				y = -200,
+				overlay = "diremaul3f"
+			},
+			{
+				x = 0,
+				y = -300,
+				overlay = "diremaul4f"
+			},
+			{
+				x = 0,
+				y = -400,
+				overlay = "diremaul5f"
+			},
+			{
+				x = 0,
+				y = -500,
+				overlay = "diremaul6f"
+			}
+		}
+	},
+	[50006] = {
+		ownerMapId = 2039,
+		entryX = 67,
+		entryY = 64,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "dragonmawretreat"
+			}
+		}
+	},
+	[50007] = {
+		ownerMapId = 2015,
+		entryX = 28,
+		entryY = 30,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "gilneascity"
+			}
+		}
+	},
+	[50008] = {
+		ownerMapId = 2010,
+		entryX = 24.38,
+		entryY = 39.8,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "gnomeregan"
+			},
+			{
+				x = 0,
+				y = -100,
+				overlay = "gnomeregan2f"
+			},
+			{
+				x = 0,
+				y = -200,
+				overlay = "gnomeregan3f"
+			},
+			{
+				x = 0,
+				y = -300,
+				overlay = "gnomeregan4f"
+			}
+		}
+	},
+	[50009] = {
+		ownerMapId = 2008,
+		entryX = 95,
+		entryY = 56,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "hateforgequarry"
+			}
+		}
+	},
+	[50010] = {
+		ownerMapId = 2009,
+		entryX = 47,
+		entryY = 74.94,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "karazhancrypt"
+			}
+		}
+	},
+	[50011] = {
+		ownerMapId = 1007,
+		entryX = 29.48,
+		entryY = 62.53,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "maraudon"
+			}
+		}
+	},
+	[50012] = {
+		ownerMapId = 1020,
+		entryX = 52.19,
+		entryY = 49.36,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "ragefire"
+			}
+		}
+	},
+	[50013] = {
+		ownerMapId = 1026,
+		entryX = 45.43,
+		entryY = 89.48,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "razorfendowns"
+			}
+		}
+	},
+	[50014] = {
+		ownerMapId = 1026,
+		entryX = 43.59,
+		entryY = 90.16,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "razorfenkraul"
+			}
+		}
+	},
+	[50015] = {
+		ownerMapId = 2025,
+		entryX = 84.3,
+		entryY = 44,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "scarletmonastery3f"
+			}
+		}
+	},
+	[50016] = {
+		ownerMapId = 2025,
+		entryX = 79.9,
+		entryY = 27.9,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "scarletmonastery4f"
+			}
+		}
+	},
+	[50017] = {
+		ownerMapId = 2025,
+		entryX = 68.2,
+		entryY = 21,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "scarletmonastery"
+			}
+		}
+	},
+	[50018] = {
+		ownerMapId = 2025,
+		entryX = 79,
+		entryY = 60,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "scarletmonastery2f"
+			}
+		}
+	},
+	[50019] = {
+		ownerMapId = 2037,
+		entryX = 69.77,
+		entryY = 73.51,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "scholomance"
+			},
+			{
+				x = 0,
+				y = -100,
+				overlay = "scholomance2f"
+			},
+			{
+				x = 0,
+				y = -200,
+				overlay = "scholomance3f"
+			},
+			{
+				x = 0,
+				y = -300,
+				overlay = "scholomance4f"
+			}
+		}
+	},
+	[50020] = {
+		ownerMapId = 2027,
+		entryX = 44.86,
+		entryY = 67.86,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "shadowfangkeep"
+			},
+			{
+				x = 0,
+				y = -100,
+				overlay = "shadowfangkeep2f"
+			},
+			{
+				x = 0,
+				y = -200,
+				overlay = "shadowfangkeep3f"
+			},
+			{
+				x = 0,
+				y = -300,
+				overlay = "shadowfangkeep4f"
+			},
+			{
+				x = 0,
+				y = -400,
+				overlay = "shadowfangkeep5f"
+			},
+			{
+				x = 0,
+				y = -500,
+				overlay = "shadowfangkeep6f"
+			},
+			{
+				x = 0,
+				y = -600,
+				overlay = "shadowfangkeep7f"
+			}
+		}
+	},
+	[50021] = {
+		ownerMapId = 2028,
+		entryX = 65.5,
+		entryY = 58.2,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "stormwindvault"
+			}
+		}
+	},
+	[50022] = {
+		ownerMapId = 2012,
+		entryX = 27.09,
+		entryY = 12.6,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "stratholme"
+			}
+		}
+	},
+	[50023] = {
+		ownerMapId = 1023,
+		entryX = 65.5,
+		entryY = 50,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "blackmorass"
+			}
+		}
+	},
+	[50024] = {
+		ownerMapId = 1023,
+		entryX = 65.5,
+		entryY = 50,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "blackmorass2f"
+			}
+		}
+	},
+	[50025] = {
+		ownerMapId = 2038,
+		entryX = 42.56,
+		entryY = 71.72,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "thedeadmines"
+			}
+		}
+	},
+	[50026] = {
+		ownerMapId = 2028,
+		entryX = 51,
+		entryY = 69.5,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "thestockade"
+			}
+		}
+	},
+	[50027] = {
+		ownerMapId = 2030,
+		entryX = 69.83,
+		entryY = 54.14,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "thetempleofatalhakkar"
+			},
+			{
+				x = 0,
+				y = -100,
+				overlay = "thetempleofatalhakkar2f"
+			},
+			{
+				x = 0,
+				y = -200,
+				overlay = "thetempleofatalhakkar3f"
+			}
+		}
+	},
+	[50028] = {
+		ownerMapId = 2004,
+		entryX = 36.74,
+		entryY = 28.16,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "uldaman"
+			}
+		}
+	},
+	[50029] = {
+		ownerMapId = 1026,
+		entryX = 54.7,
+		entryY = 66.9,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "wailingcaverns"
+			}
+		}
+	},
+	[50030] = {
+		ownerMapId = 1023,
+		entryX = 38.73,
+		entryY = 19.99,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "zulfarrak"
+			}
+		}
+	},
+	[60001] = {
+		ownerMapId = 1021,
+		entryX = 15.53,
+		entryY = 98.02,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "ahnqiraj"
+			}
+		}
+	},
+	[60002] = {
+		ownerMapId = 1021,
+		entryX = 15.53,
+		entryY = 98.02,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "ahnqiraj2f"
+			}
+		}
+	},
+	[60003] = {
+		ownerMapId = 50003,
+		entryX = 64,
+		entryY = 71.5,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "blackwinglair"
+			},
+			{
+				x = 0,
+				y = -100,
+				overlay = "blackwinglair2f"
+			},
+			{
+				x = 0,
+				y = -200,
+				overlay = "blackwinglair3f"
+			},
+			{
+				x = 0,
+				y = -300,
+				overlay = "blackwinglair4f"
+			}
+		}
+	},
+	[60004] = {
+		ownerMapId = 1015,
+		entryX = 18,
+		entryY = 54.5,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "emeraldsanctum"
+			}
+		}
+	},
+	[60005] = {
+		ownerMapId = 50002,
+		entryX = 38.3,
+		entryY = 36.4,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "moltencore"
+			}
+		}
+	},
+	[60006] = {
+		ownerMapId = 2012,
+		entryX = 39.93,
+		entryY = 25.87,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "naxxramas"
+			}
+		}
+	},
+	[60007] = {
+		ownerMapId = 1010,
+		entryX = 52.41,
+		entryY = 76.39,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "onyxiaslair"
+			}
+		}
+	},
+	[60008] = {
+		ownerMapId = 1021,
+		entryX = 58.67,
+		entryY = 14.03,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "ruinsofahnqiraj"
+			}
+		}
+	},
+	[60009] = {
+		ownerMapId = 2012,
+		entryX = 39.93,
+		entryY = 25.87,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "naxxramas2f"
+			}
+		}
+	},
+	[60010] = {
+		ownerMapId = 2009,
+		entryX = 47,
+		entryY = 74.94,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "karazhan"
+			}
+		}
+	},
+	[60011] = {
+		ownerMapId = 2009,
+		entryX = 47,
+		entryY = 74.94,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "upperkarazhan"
+			},
+			{
+				x = 0,
+				y = -100,
+				overlay = "upperkarazhan2f"
+			}
+		}
+	},
+	[60012] = {
+		ownerMapId = 2029,
+		entryX = 53.81,
+		entryY = 17.57,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "zulgurub"
+			}
+		}
+	},
+	[70001] = {
+		ownerMapId = nil,
+		entryX = nil,
+		entryY = nil,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "alteracvalley"
+			},
+			{
+				x = 0,
+				y = -100,
+				overlay = "alteracvalleyclassic"
+			}
+		}
+	},
+	[70002] = {
+		ownerMapId = nil,
+		entryX = nil,
+		entryY = nil,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "arathibasin"
+			}
+		}
+	},
+	[70003] = {
+		ownerMapId = nil,
+		entryX = nil,
+		entryY = nil,
+		floors = {
+			{
+				x = 0,
+				y = 0,
+				overlay = "warsonggulch"
+			}
+		}
+	}
+}
